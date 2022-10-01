1674cdc01d225bc4cb569ea5400b1b2de3468a91
Isaiah Keller <isaiah.keller@gmail.com>
2022-10-01 17:43:22 +0000

    added dependencies to package.json

---
+++ src/routes/v1.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const dataModules = require('../models');
+
+const router = express.Router();
+
+router.param('model', (req, res, next) => {
+
+});
