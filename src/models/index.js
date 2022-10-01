6f9f7aee1578774a0af36cdc1410e95d91718e38
Timothee Odushina <hsp20tim@gmail.com>
2022-10-01 20:44:05 +0000

    working on users.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const userModel = require('./users');
+// const userModel = require('./users');
 const pizzaModel = require('./pizza/model');
 const Collection = require('./data-collection');
+require('dotenv').config();
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 let options = { logging: false };
 const sequelize = new Sequelize(DATABASE_URL);
 
-const user = userModel(sequelize, DataTypes);
+// const user = userModel(sequelize, DataTypes);
 const pizza = pizzaModel(sequelize, DataTypes);
 
 module.exports = {
   database: sequelize,
   pizza: new Collection(pizza),
-  user,
+//   user,
 };
