0bc12b300201206df5043d144a2cbafa491ba1bb
Isaiah Keller <isaiah.keller@gmail.com>
2022-10-01 20:19:06 +0000

    added v2

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const userModel = require('./users');
+// const userModel = require('./users');
 const pizzaModel = require('./pizza/model');
 const Collection = require('./data-collection');
 
@@ -9,11 +9,11 @@ const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 let options = { logging: false };
 const sequelize = new Sequelize(DATABASE_URL);
 
-const user = userModel(sequelize, DataTypes);
+// const user = userModel(sequelize, DataTypes);
 const pizza = pizzaModel(sequelize, DataTypes);
 
 module.exports = {
   database: sequelize,
   pizza: new Collection(pizza),
-  user,
+  // user,
 };
